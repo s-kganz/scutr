f46a30cf774779a21a0a19d9fa990510754906fe
s-kganz <ganzkeenan1@gmail.com>
2021-01-12 09:18:40 -0800

    add wine toydata

---
+++ R/data.R
@@ -0,0 +1,4 @@
+#' Type and chemical analysis of three different kinds of wine.
+#' @format a data.frame with 178 rows and 14 columns
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/Wine}
+"wine"
