4c8818bd226bdfdec18e94cc5851d7708a1217a5
s-kganz <ganzkeenan1@gmail.com>
2021-01-12 09:42:28 -0800

    test that sampling produces correct number of rows

--- tests/testthat/test-scut.R
+++ tests/testthat/test-scut.R
@@ -1,5 +1,24 @@
-context("Undersampling")
+data("wine")
+wine$type <- as.factor(wine$type)
 
-test_that("mclust reduces number of samples", {
-    expect_equal(2, 2)
+context("Undersampling")
+m <- 50
+undersamp <- .do.undersample(wine, "mclust", "type", 2, m)
+test_that("mclust produces correct number of samples", {
+    expect_equal(nrow(undersamp), m)
+})
+undersamp <- .do.undersample(wine, "random", "type", 2, m)
+test_that("random produces correct number of samples", {
+    expect_equal(nrow(undersamp), m)
+})
+
+context("Oversampling")
+m <- 70
+oversamp <- .do.oversample(wine, "SMOTE", "type", 3, m)
+test_that("SMOTE produces correct number of samples", {
+    expect_equal(nrow(oversamp), m)
+})
+oversamp <- .do.oversample(wine, "random", "type", 3, m)
+test_that("SMOTE produces correct number of samples", {
+    expect_equal(nrow(oversamp), m)
 })
