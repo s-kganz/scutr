90be8a14175a8b6462c3926667be43ab77ce5a38
s-kganz <ganzkeenan1@gmail.com>
2021-01-18 10:05:19 -0800

    add imbalance dataset documentation

--- R/data.R
+++ R/data.R
@@ -2,3 +2,9 @@
 #' @format a data.frame with 178 rows and 14 columns
 #' @source \url{https://archive.ics.uci.edu/ml/datasets/Wine}
 "wine"
+
+#' An imbalanced dataset with randomly placed normal distributions around the origin.
+#' The nth class has n * 10 observations.
+#' @format a data.frame with 2100 rows and 11 columns
+#' @source \url{https://gist.github.com/s-kganz/d08473f9492d48ea0e56c3c8a3fe1a74}
+"imbalance"
