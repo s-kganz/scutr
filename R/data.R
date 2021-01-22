1dc5855410a21ac9f3dc07dae59107dbed3b81cb
s-kganz <ganzkeenan1@gmail.com>
2021-01-22 12:07:05 -0800

    add bullseye data to demo tomek undersampling

--- R/data.R
+++ R/data.R
@@ -8,3 +8,8 @@
 #' @format a data.frame with 2100 rows and 11 columns
 #' @source \url{https://gist.github.com/s-kganz/d08473f9492d48ea0e56c3c8a3fe1a74}
 "imbalance"
+
+#' An imbalanced dataset with a minor class centered around the origin with a majority class surrounding the center.
+#' @format a data.frame with 1000 rows and 3 columns.
+#' @source \url{https://gist.github.com/s-kganz/c2534666e369f8e19491bb29d53c619d}
+"bullseye"
