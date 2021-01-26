564b1a53a51580cf76ad9c852f1804a222f08dc1
s-kganz <ganzkeenan1@gmail.com>
2021-01-25 20:00:52 -0800

    make scut testing test every combination of under/oversampling

--- tests/testthat/test-scut.R
+++ tests/testthat/test-scut.R
@@ -1,15 +1,20 @@
 context("scut")
-scutted <- SCUT(type ~ ., wine)
-test_that("SCUT results have equal class distribution", {
-    counts <- table(scutted$type)
-    expect_true(all(counts == counts[[1]]))
-})
-# use kmeans in the parallel version so it completes in a reasonable amount of time
-scutted <- SCUT.parallel(class ~ ., imbalance, undersample="kmeans", ncores=2)
-test_that("SCUT.parallel results have equal class distribution", {
-    counts <- table(scutted$class)
-    expect_true(all(counts == counts[[1]]))
-})
+for (osamp in c("SMOTE", "random")){
+    for (usamp in c("mclust", "kmeans", "hclust", "mindist", "tomek")){
+        scutted <- SCUT(type ~ ., wine, oversample=osamp, undersample=usamp)
+        counts <- table(scutted$type)
+        test_that(paste(osamp, usamp, "have equal class distribution"), {
+            expect_true(all(counts == counts[[1]]))
+        })
+        # skip mclust in parallel test because it's really slow
+        # if (usamp == "mclust") next
+        # scutted <- SCUT.parallel(type ~ ., wine, oversample=osamp, underample=usamp)
+        # counts <- table(scutted$type)
+        # test_that(paste(osamp, usamp, "have equal class distribution in parallel version"), {
+        #     expect_true(all(counts == counts[[1]]))
+        # })
+    }
+}
 
 # dummy func that follows the required signature
 foo <- function(data, cls, cls.col, m){
@@ -20,4 +25,11 @@ scutted <- SCUT(type ~ ., wine, undersample=foo)
 test_that("Custom functions are valid with SCUT", {
     counts <- table(scutted$type)
     expect_true(all(counts == counts[[1]]))
+})
+
+scutted <- SCUT(type ~ ., wine, undersample="kmeans", usamp.opts = list(k=7))
+test_that("Custom arguments are passed correctly", {
+    counts <- table(scutted$type)
+    expect_true(all(counts == counts[[1]]))
+    expect_error(SCUT(type ~ ., wine, osamp.opts=list(k=7)))
 })
