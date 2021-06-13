8901497c661c7da9b0ef9979806435de36e051ed
s-kganz <ganzkeenan1@gmail.com>
2021-06-13 12:18:36 -0700

    refactor testing script to reduce unnecessary tests

--- tests/testthat/test-scut.R
+++ tests/testthat/test-scut.R
@@ -13,48 +13,70 @@ if (nzchar(chk) && chk == "TRUE") {
 }
 
 scutenv <- as.environment("package:scutr")
-oversamplers <- c(ls(envir = scutenv, pattern = "oversample_*"), "resample_random")
-undersamplers <- c(ls(envir = scutenv, pattern = "undersample_*"), "resample_random")
-for (osamp in oversamplers) {
-  for (usamp in undersamplers) {
-    test_that(paste(osamp, usamp, "have equal class distribution"), {
-      scutted <- SCUT(wine, "type", oversample = get(osamp), undersample = get(usamp))
-      expect_true(strictly_balanced(scutted$type))
-    })
+oversamplers  <- ls(envir = scutenv, pattern = "oversample_*")
+undersamplers <- ls(envir = scutenv, pattern = "undersample_*")
+n_oversamplers  <- length(oversamplers)
+n_undersamplers <- length(undersamplers)
 
-
-    test_that(paste(osamp, usamp, "(parallel) have equal class distribution"), {
-      # Parallel version fails on windows if the package is not installed. Since this is the
-      # case during testing, we skip the parallel version.
-      skip_on_os("windows")
-
-
-      scutted <- SCUT_parallel(wine, "type",
-        ncores = ncores, oversample = get(osamp),
-        undersample = get(usamp)
-      )
-
-      expect_true(strictly_balanced(scutted$type))
-    })
-  }
+# Test each sampling method in turn, using random as the other method
+oversamplers <- c(oversamplers, rep("resample_random", n_undersamplers))
+undersamplers <- c(undersamplers, rep("resample_random", n_oversamplers))
+if (length(oversamplers) != length(undersamplers)) {
+  stop("Testing setup should have equal number of under/oversamplers.")
 }
 
+for (i in 1:length(undersamplers)) {
+  osamp <- oversamplers[i]
+  usamp <- undersamplers[i]
+
+  test_that(paste(osamp, usamp, "have equal class distribution"), {
+    scutted <-
+      SCUT(wine,
+           "type",
+           oversample = get(osamp),
+           undersample = get(usamp))
+    expect_true(strictly_balanced(scutted$type))
+  })
+
+  test_that(paste(osamp, usamp, "(parallel) have equal class distribution"),
+            {
+              # Parallel version fails on windows if the package is not installed.
+              # Since this is the case during testing, we skip the parallel version.
+              skip_on_os("windows")
+
+
+              scutted <- SCUT_parallel(
+                wine,
+                "type",
+                ncores = ncores,
+                oversample = get(osamp),
+                undersample = get(usamp)
+              )
+
+              expect_true(strictly_balanced(scutted$type))
+            })
+}
+
+# test custom functions
 # dummy func that follows the required signature
 foo <- function(data, cls, cls_col, m) {
-  subset <- data[data[[cls_col]] == cls, ]
-  subset[rep(1, m), ]
+  subset <- data[data[[cls_col]] == cls,]
+  subset[rep(1, m),]
 }
 scutted <- SCUT(wine, "type", undersample = foo)
 test_that("Custom functions can be passed", {
   expect_true(strictly_balanced(scutted$type))
 })
 
-scutted <- SCUT(wine, "type", undersample = undersample_kmeans, usamp_opts = list(k = 7))
+scutted <-
+  SCUT(wine,
+       "type",
+       undersample = undersample_kmeans,
+       usamp_opts = list(k = 7))
 test_that("Custom arguments are passed correctly", {
   expect_true(strictly_balanced(scutted$type))
   expect_error(SCUT(wine, "type", osamp_opts = list(k = 7)),
-    regexp = "unused argument*"
-  )
+               regexp = "unused argument*")
 })
 
 context("bad input")
@@ -62,18 +84,15 @@ bad <- wine
 bad$x1 <- as.character(bad$x1)
 test_that("Non numeric columns are not allowed", {
   expect_error(SCUT(bad, "type"),
-    regexp = "*Data frame must be only numeric*"
-  )
+               regexp = "*Data frame must be only numeric*")
 })
 test_that("Class column must be present", {
   expect_error(SCUT(bad, "foo"),
-    regexp = "Column not found in data*"
-  )
+               regexp = "Column not found in data*")
 })
 bad$x1 <- as.numeric(bad$x1)
 bad$x1[1] <- NA
 test_that("NAs are not allowed", {
   expect_error(SCUT(bad, "type"),
-    regexp = "Data frame cannot contain NAs."
-  )
+               regexp = "Data frame cannot contain NAs.")
 })
